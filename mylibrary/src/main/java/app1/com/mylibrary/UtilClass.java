74b7c85e0e5587cbde24f0fb43e328cdfb7774c6
thuannmvng <thuannm@vng.com.vn>
2017-10-16 14:36:20 +0700

    Test jar
    test

--- mylibrary/src/main/java/app1/com/mylibrary/UtilClass.java
+++ mylibrary/src/main/java/app1/com/mylibrary/UtilClass.java
@@ -1,9 +1,14 @@
 package app1.com.mylibrary;
 
+import android.util.Log;
+
 /**
  * Created by CPU10329-local on 10/16/2017.
  */
 
 public class UtilClass {
 
+    public void testJarLib(){
+        Log.i("debuglog", "aaaa");
+    }
 }
