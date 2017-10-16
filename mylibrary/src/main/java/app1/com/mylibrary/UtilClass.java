62acaa837a9d402129d08e4bfadd17b6d3828caf
thuannmvng <thuannm@vng.com.vn>
2017-10-16 16:35:50 +0700

    test lib

--- mylibrary/src/main/java/app1/com/mylibrary/UtilClass.java
+++ mylibrary/src/main/java/app1/com/mylibrary/UtilClass.java
@@ -2,6 +2,8 @@ package app1.com.mylibrary;
 
 import android.util.Log;
 
+import com.zing.zalo.zalosdk.oauth.ZaloSDK;
+
 /**
  * Created by CPU10329-local on 10/16/2017.
  */
@@ -10,5 +12,6 @@ public class UtilClass {
 
     public void testJarLib(){
         Log.i("debuglog", "aaaa");
+//        ZaloSDK.Instance
     }
 }
